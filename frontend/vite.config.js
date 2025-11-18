5f3cb90bf5f3c7ae9b5197c8e887b12d29a034ab
Aditya <adityajogdand15@gmail.com>
2025-11-18 13:04:52 +0530

    frontedn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() ,   tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+  // server: {
+  //   host: '172.16.1.62',   // Replace with your system's IP address
+  //   port: 5173,            // Set the desired port
+  //   strictPort: true,      // Ensure that the port is strictly used (if 5173 is taken, it will fail)
+  // },
 })
